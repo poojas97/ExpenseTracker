f037cf6023b9b09ac1e5d10c7727fdf8daafcbed
pooja <pooja.sridhar@freshworks.com>
2018-12-27 18:04:18 +0530

    validate user

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+	validates :name,presence: true, uniqueness: { case_sensitive: false },
+	length: {minimum: 3, maximum: 50}
+
+	VALID_EMAIL_REGEX =
+	 /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	validates :email, presence: true, uniqueness: { case_sensitive: false },
+	length: {maximum: 105}, format: { with: VALID_EMAIL_REGEX }
+
+
+
+
+end
