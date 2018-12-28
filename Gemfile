97f9d42b492f8d7b2be84d387c34e920897aa3ea
pooja <pooja.sridhar@freshworks.com>
2018-12-28 15:33:05 +0530

    added validations in model

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 
+gem 'validates_timeliness', '~> 5.0.0.alpha3'
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,8 +1,18 @@
+require "date"
+
 class Expense <ActiveRecord::Base
 	validates :expense_name, presence: true, length: { minimum: 3, maximum: 50} 
 	validates :amount, presence: true
     # validates_date :date, presence: true, on_or_before: lambda { Date.current }
     #validates :date, timeliness: { on_or_before: lambda { Date.current }, type: :date }
+    # validates  :date , date: true
+    validates_date :date, :on_or_before => lambda { Date.today}
+  #   validate :happened_at_is_valid_datetime
 
+  # def happened_at_is_valid_datetime
+  #    if !date.is_a?(Date)
+  #     errors.add(:date, 'must be a valid date') 
+  #   end
+  # end
 
 end
